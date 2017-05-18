906358373f83327ce818322445c1486118eb7d5f
venu-gopal-vallepu <venugopalvallepub4@gmail.com>
2017-05-19 01:10:33 +0530

    first commit

--- test/validationScenario/testLogin.js
+++ test/validationScenario/testLogin.js
@@ -2,11 +2,15 @@ describe("testLogin", function() {
     var wait = 1000;
     it("should pass valid credentials", function() {
         //ST.wait(8000);
+        
         ST.field('//input[@name="username"]').setValue('divya.aasireddy@walkingtree.in');
         ST.field('//input[@name="password"]').setValue('testing').wait(1000);
         divEl = //img[@data-qtip='FAQ'];
-        ST.field("//a[contains(@class,'x-btn signintext')]").click(10,10).wait(function(divEl, done){
+        ST.field("//a[contains(@class,'x-btn signintext')]").click(10,10).wait(3000).and(function(divEl, done){
             divEl.hasCls('forum faq');
+            expect(1).toBe(2);
+            console.log('resuts');
+            done();
         });
     });
 });
